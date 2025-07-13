c7327cd8d640ff17d519fb2f2dc48236f637bbf1
erennali <eren_ali_koca@hotmail.com>
2025-07-13 15:56:29 +0300

    feat: created new model for operations enum

--- Calculator/Managers/CalculatorManager.swift
+++ Calculator/Managers/CalculatorManager.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-// MARK: - Operation
-enum Operation: String, CaseIterable {
-    case add = "+", subtract = "-", multiply = "*", divide = "/"
-    
-    func calculate(_ inputL: Double, _ inputR: Double) -> Double {
-        switch self {
-        case .add: return inputL + inputR
-        case .subtract: return inputL - inputR
-        case .multiply: return inputL * inputR
-        case .divide: return inputR != 0 ? inputL / inputR : .nan
-        }
-    }
-}
-
 final class CalculatorManager {
     
 }
---
+++ Calculator/Models/Calculator.swift
@@ -0,0 +1,42 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Eren Ali Koca on 13.07.2025.
+//
+
+import Foundation
+
+enum Input: Int, CaseIterable {
+    case zero = 0, one, two, three, four, five, six, seven, eight, nine
+    case dot, clear, sign, percent, divide, multiply, subtract, add, equals
+    
+    var symbol: String {
+            if isDigit { return "\(rawValue)" }
+            return [
+                10: ".",
+                11: "AC",
+                12: "±",
+                13: "%",
+                14: "÷",
+                15: "×",
+                16: "-",
+                17: "+",
+                18: "="
+            ][rawValue] ?? ""
+        }
+    
+      var isDigit: Bool { rawValue < 10 }
+      var isOperator: Bool { (14...17).contains(rawValue) }
+      
+      func apply(_ inputL: Decimal, _ inputR: Decimal) -> Decimal? {
+          switch self {
+          case .add: inputL + inputR
+          case .subtract: inputL - inputR
+          case .multiply: inputL * inputR
+          case .divide: inputR.isZero ? nil : inputL / inputR
+          default: inputR
+          }
+      }
+  }
+    
